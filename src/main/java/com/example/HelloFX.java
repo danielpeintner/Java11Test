005f6ffeb853050c1a48d968d938621653371bf4
danielpeintner <daniel@peintner.org>
2019-04-12 10:19:41 +0200

    feat: add TableView with TableFilter

--- src/main/java/com/example/HelloFX.java
+++ src/main/java/com/example/HelloFX.java
@@ -7,10 +7,14 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import org.controlsfx.control.HyperlinkLabel;
+import org.controlsfx.control.table.TableFilter;
 
 public class HelloFX extends Application {
 
@@ -20,6 +24,7 @@ public class HelloFX extends Application {
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         BorderPane bp = new BorderPane();
+        // Top
         bp.setTop(l);
         HyperlinkLabel hyperlinkLabel = new HyperlinkLabel("[Link]");
         hyperlinkLabel.setOnAction(event -> {
@@ -35,14 +40,71 @@ public class HelloFX extends Application {
                     break;
             }
         });
-        bp.setCenter(hyperlinkLabel);
+        // Center
+        bp.setCenter(getTableView());
+        // Bottom
+        bp.setBottom(hyperlinkLabel);
+
         Scene scene = new Scene(bp, 640, 480);
         stage.setScene(scene);
         stage.show();
+    }
+
+    TableView getTableView() {
+        TableView tableView = new TableView();
+
+        TableColumn<String, Person> column1 = new TableColumn<>("First Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+
+
+        TableColumn<String, Person> column2 = new TableColumn<>("Last Name");
+        column2.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+
+
+        tableView.getColumns().add(column1);
+        tableView.getColumns().add(column2);
+
+        tableView.getItems().add(new Person("John", "Doe"));
+        tableView.getItems().add(new Person("Jane", "Deer"));
+
+        // Issue
+        TableFilter<Person> tableFilter = TableFilter.forTableView(tableView).apply();
+
+        return tableView;
     }
 
     public static void main(String[] args) {
         launch();
     }
 
+
+    public static class Person {
+
+        private String firstName = null;
+        private String lastName = null;
+
+        public Person() {
+        }
+
+        public Person(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+    }
 }

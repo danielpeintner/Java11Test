b33e85f55000a3b6775657fe15a53aa21627ba97
danielpeintner <daniel.peintner@gmail.com>
2019-12-05 13:32:19 +0100

    feat: add junit test

---
+++ src/test/java/com/example/hellofx/HelloFXTest.java
@@ -0,0 +1,14 @@
+package com.example.hellofx;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloFXTest {
+
+    @Test
+    public void test1() {
+        assertEquals(1, 1);
+    }
+
+}
